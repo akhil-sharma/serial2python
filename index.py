1c88ac3485b1a28fa1c591b6cae6368d05d59d9a
Akhil Sharma <akhilsharma.cse@gmail.com>
2022-02-23 21:09:50 -0500

    added comments.

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
-import serial
-import requests
+# importing required modules.
+import serial # Reading to and writing from the serial stream
+import requests # Sending web requests
 
+# contants
+# TODO: Modify based on your settings. 
 PORT_NAME = 'COM3'
 BAUDRATE  = 9600
 TIMEOUT = .1
 WEBHOOK_URL = "https://maker.ifttt.com/trigger/soil_moisture_changed/json/with/key/pTWdShVd6Lx8Bg6SDh8Wo7gApQ3wR1achlY-ke1oYUT"
 
+# Simple method for sending the web request to the IFTTT webhook. 
 def handleRawData(data: str) -> None:
     number, name = data.strip().split(" ")
     payload = {
@@ -15,11 +19,14 @@ def handleRawData(data: str) -> None:
     response = requests.post(WEBHOOK_URL, payload)
     print(response.text)
     
-
+# Open the serial port.
 arduino = serial.Serial(port=PORT_NAME, baudrate=BAUDRATE, timeout=TIMEOUT)
+
+# Clear the input and output streams. 
 arduino.flushInput()
 arduino.flushOutput()
 
+# Continuously read the data stream on the serial port.
 while True:
   data_raw = arduino.readline()
   if data_raw:
